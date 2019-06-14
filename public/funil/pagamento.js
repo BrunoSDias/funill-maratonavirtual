cc3dbf1487852a871a003b93cee5b0aceb8c6f42
Danilo <didox_59@yahoo.com.br>
2019-06-14 10:34:07 -0300

    commit
    correcao

--- public/funil/pagamento.js
+++ public/funil/pagamento.js
@@ -703,12 +703,9 @@ function eraseCookie(name) {
     document.cookie = name+'=; Max-Age=-99999999;';  
 }
 
-function IsEmail(email){
-  var exclude=/[^@-.w]|^[_@.-]|[._-]{2}|[@.]{2}|(@)[^@]*1/;
-  var check=/@[w-]+./;
-  var checkend=/.[a-zA-Z]{2,3}$/;
-  if(((email.search(exclude) != -1)||(email.search(check)) == -1)||(email.search(checkend) == -1)){return false;}
-  else {return true;}
+function IsEmail(email) {
+  var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
 }
 
 $(function(){
