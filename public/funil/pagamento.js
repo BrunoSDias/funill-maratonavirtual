b1021084026d39252df86bab3e79f280afa1c12c
Danilo Aparecido dos Santos <didox_59@yahoo.com.br>
2020-07-17 22:18:31 -0300

    fix

--- public/funil/pagamento.js
+++ public/funil/pagamento.js
@@ -999,7 +999,7 @@ function eraseCookie(name) {
 }
 
 function IsEmail(email) {
-  var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  var re = /\S+@\S+\.\S+/;
   return re.test(String(email).toLowerCase());
 }
 
